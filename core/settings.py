9d4bc457b208799a001d226b185e7a74c3c43ab1
MrSiddu73 <siddusnyamagoud27@gmail.com>
2026-01-16 18:23:57 +0530

    Add fallback SECRET_KEY for CI tests

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-ci-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
